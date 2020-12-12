98b5917c24feed3cd00b95163837f6289a9f323f
fkdlalsem <as5869bc@gmail.com>
2020-12-13 00:21:03 +0900

    주석 생성

--- src/this_able/Sort.java
+++ src/this_able/Sort.java
@@ -6,38 +6,38 @@ public class Sort {
 	static String recommend[][][] = new String[100][20][2];
 	static String temp;
 	
-	 static void re_sort(int i){
+	 static void re_sort(int round){						//그직업에 점수 넣고 직업이름 넣는 배열 생성
 		 z=0;
-		 for(int a=2;a<16;a++) {
-			 if(Utill.cheak[i][a] == 0) {
-				 recommend[i][z][0] = String.valueOf(Utill.point[i][a]);
-				 recommend[i][z][1] = csv_file.indat[a][0];
+		 for(int a=2;a<16;a++) {												
+			 if(Utill.cheak[round][a] == 0) {				//이 직업이 나에게 맞지 않는다면 아예 생성이 안됌
+				 recommend[round][z][0] = String.valueOf(Utill.point[round][a]);
+				 recommend[round][z][1] = csv_file.indat[a][0];
 				 z=z+1;
 			 }
 			 
 		 }
 		 all=z;
-		 bubble_sort(i);
+		 bubble_sort(round);
 	 }
 	 
-	 static void bubble_sort(int i) {
+	 static void bubble_sort(int round) {				//정렬시작
 		 System.out.println("\n\n\n\n\n\n");
 		 for(z=0;z<all-2;z++) {
 			 for(int up = 0;up<all-1;up++) {
-				 if(Integer.parseInt(recommend[i][up][0]) > Integer.parseInt(recommend[i][up+1][0])) {
-					    temp = recommend[i][up][0];
-					    recommend[i][up][0] = recommend[i][up+1][0];
-					    recommend[i][up+1][0] = temp;
+				 if(Integer.parseInt(recommend[round][up][0]) > Integer.parseInt(recommend[round][up+1][0])) {
+					    temp = recommend[round][up][0];												//점수에 맞게 정렬
+					    recommend[round][up][0] = recommend[round][up+1][0];				
+					    recommend[round][up+1][0] = temp;
 					    
-					    temp = recommend[i][up][1];
-					    recommend[i][up][1] = recommend[i][up+1][1];
-					    recommend[i][up+1][1] = temp;
+					    temp = recommend[round][up][1];												//직업도 같이 정렬해줌
+					    recommend[round][up][1] = recommend[round][up+1][1];
+					    recommend[round][up+1][1] = temp;
 				 }
 			 }
 		 }
 		 System.out.println("추천할 직업의 수는 " + all +"개 입니다.\n");
 		 for(z=all-1;z>-1;z--) {
-			 System.out.println(recommend[i][z][1] +"   " + recommend[i][z][0] + "점");
+			 System.out.println(recommend[round][z][1] +"   " + recommend[round][z][0] + "점");
 		 }
 	 }
 }
--- src/this_able/Utill.java
+++ src/this_able/Utill.java
@@ -5,21 +5,22 @@ import java.util.Scanner;
 public class Utill {
 	static int answer;
 	static int i;
-	static char information;
-	static int round = -1;
-	static char mine[][] = new char[10][10];
-	static String x = "x";
+	static int c;
+	static char information[] = new char[8];					//나의 정보를 입력하는 배열
+	static int round = -1;									//직업 추천을 한 횟수를 담는 변수
+	static char mine[][] = new char[10][10];				//나의 정보를 담는 배열
+	static String x = "x";					
 	static String o = "o";
-	static int point[][] = new int[100][20];
-	static int cheak[][] = new int[100][16];
-	 static void clearConsole(){
+	static int point[][] = new int[100][20];				//점수를 담는 배열
+	static int cheak[][] = new int[100][16];				//직업이 맞는지 안맞는지 체크하는 배열
+	 static void clearConsole(){		//화면 지우는 함수
 		 for(i=0;i<15;i++) {
 			 System.out.println("\n");
 		 }
 	 }
 	 
 
-	 static void loby() {
+	 static void loby() {  									//로비창
 		 System.out.println("당신은 무엇을 할것입니까?"+ "\n");
 			System.out.println("1. 직업 추천!" + "\n");
 			System.out.println("2. 직업 추천 목록" + "\n");
@@ -65,39 +66,46 @@ public class Utill {
 				}
 	 }
 
-	 static void my_information(){
+	 static void my_information(){																	//나의 신체정보를 입력받는 함수
 		 int a;
 		 round=round+1;
+		 if(round != 0) {																			// 또사용하기위한 초기화
+			 for(a=1;a<8;a++)
+				 information[a] = ' ';
+		 }
 		 for(a=1;a<8;a++) {
+			 for(c=0; information[a]!= 'r' && information[a]!= 'o' && information[a]!= 'x' ;c++) {		//답변을 o,x,r만 받을수 있게 하는 반복문
 			 System.out.println(csv_file.indat[1][a]+"\n" +"(있으면 o, 없으면 x 나가고싶으면 r)"+"\n");
 			 Scanner scanner = new Scanner(System.in);
-			 information = scanner.next().charAt(0);
-			 if(information == 'r') {
+			 information[a] = scanner.next().charAt(0);
+			 }
+			 if(information[a] == 'r') {
 				 return;
 			 }
-			 mine[round][a] = information;
+			 mine[round][a] = information[a];														//입력받은 정보를 나의 정보를 담는 배열에 저장
 		 	}
 		 Comparison(round);
-	 	}
+		 	
+		 }
 	 
-	 static void Comparison(int i) {
+	 static void Comparison(int round) {
 		 System.out.println("\n\n\n\n\n\n\n\n\n");
 		 for(int a=2;a<16;a++) {
 			 for(int b=1;b<8;b++) {
-				 if((csv_file.indat[a][b].equals(o))&&(Character.toString(Utill.mine[i][b]).equals(x))) {
-					 point[i][a] = point[i][a]+1;
+				 if((csv_file.indat[a][b].equals(o))&&(Character.toString(Utill.mine[round][b]).equals(x))) {		//나에게 딱맞는 직업은 점수 +
+					 point[round][a] = point[round][a]+1;
 				 }
 				 
-				 if((csv_file.indat[a][b].equals(x))&&(Character.toString(Utill.mine[i][b]).equals(o))) {
-					 cheak[i][a] = 1;
+				 if((csv_file.indat[a][b].equals(x))&&(Character.toString(Utill.mine[round][b]).equals(o))) {		//직업이 맞지 않는 다면 1로 표시하고 반복문을 멈쳐 다음 배열로 이동
+					 cheak[round][a] = 1;
 					 break;
 				 }
 				 else {
-					 cheak[i][a] = 0;
+					 cheak[round][a] = 0;																			//직업이 맞으면 0으로 표시
 				 }
 				 
 			 }
 		 }
-		 Sort.re_sort(i);
+		 Sort.re_sort(round);
 	}
 }
